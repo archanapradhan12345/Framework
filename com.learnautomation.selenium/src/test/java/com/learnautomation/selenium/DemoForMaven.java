2aed0846ff6e568efe504cb17e0b28f1ebdba373
archanapradhan12345 <haristhkumar259@gmail.com>
2023-06-12 13:04:22 +0100

    Code pushed

--- com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/DemoForMaven.java
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/DemoForMaven.java
@@ -4,7 +4,9 @@ public class DemoForMaven {
 	
 	public void testHello() {
 		System.out.println("Welcome to Maven world"); 
-		System.out.println("Welcome to GIT world"); 
+		System.out.println("Welcome to GIT world");
+		System.out.println("Welcome to GIT world");
+		
 		
 	}
 
